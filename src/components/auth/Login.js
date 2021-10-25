dce4dcee2a4f6db75d7a9432d612c6bb8d276db7
Luis Cruz <luisccc99@gmail.com>
2021-10-25 00:09:04 -0600

    Add basic layout to Login page

--- src/components/auth/Login.js
+++ src/components/auth/Login.js
@@ -1,1 +1,68 @@
 import React from 'react';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+import TextField from '@mui/material/TextField';
+import Container from '@mui/material/Container';
+import Button from '@mui/material/Button';
+
+const Login = () => {
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        const formData = new FormData(event.currentTarget);
+        console.log(formData.get('correo'), formData.get('clave'));
+    };
+
+    return (
+        <Container maxWidth="xs">
+            <Box
+                sx={{
+                    marginTop: 7,
+                    display: 'flex',
+                    flexDirection: 'column',
+                    alignItems: 'center',
+                }}>
+                <Typography component="h1" variant="h5">
+                    Login
+                </Typography>
+                <Box
+                    component="form"
+                    onSubmit={handleSubmit}
+                    noValidate
+                    sx={{ mt: 1 }}>
+                    <TextField
+                        margin="normal"
+                        required
+                        fullWidth
+                        id="correo"
+                        label="Correo"
+                        name="correo"
+                        autoComplete="email"
+                        type="email"
+                        autoFocus
+                    />
+
+                    <TextField
+                        margin="normal"
+                        required
+                        fullWidth
+                        name="clave"
+                        label="Clave"
+                        type="password"
+                        autocomplete="current-password"
+                    />
+                    <Button
+                        type="submit"
+                        fullWidth
+                        variant="contained"
+                        sx={{ mt: 2, mb: 3 }}>
+                        Iniciar Sesion
+                    </Button>
+                </Box>
+            </Box>
+        </Container>
+    );
+};
+
+
+export default Login;
