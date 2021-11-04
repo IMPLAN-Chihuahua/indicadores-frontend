c0469151de1a3f24f20b74ab9dc7ecc9ac9fdf70
Luis Cruz <luisccc99@gmail.com>
2021-11-03 23:15:05 -0700

    Add register form with routing to login and basic validation with react hook form

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from 'react-router-dom';
 import Login from './components/auth/Login';
+import Register from './components/auth/Register';
 
 function App() {
   return (
     <div className="App">
-      <Login />
+      <Router>
+        <Switch>
+          <Route path="/login">
+            <Login />
+          </Route>
+          <Route path="/register">
+            <Register />
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
