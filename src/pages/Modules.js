b8279df8f7b28ab7f9c028b1556e6791d98e1420
Miguel Valdez <56283510+MiquelVald@users.noreply.github.com>
2022-03-01 10:13:10 -0700

    Example of how to call useState of modal into the parent page

--- src/pages/Modules.js
+++ src/pages/Modules.js
@@ -1,7 +1,22 @@
-import React from 'react'
+import { Button, Typography } from '@mui/material'
+import React, { useState } from 'react'
 import DatagridTable from '../components/dashboard/common/DatagridTable'
+import FormModal from '../components/dashboard/common/FormModal'
+
 export const Modules = () => {
+  const [openModal, setOpenModal] = useState(false);
+  const handleModal = () => setOpenModal(prev => !prev);
   return (
-    <DatagridTable/>
+    <>
+      <div>
+        <Button onClick={handleModal}>Click me</Button>
+        <Button onClick={handleModal}>Click me</Button>
+      </div>
+      <FormModal open={openModal} setOpenModal={setOpenModal} title={'Editar módulo'}>
+        <Typography sx={{ color: 'black', fontWeight: 'bold' }}>
+          Hello worldaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaworldaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaworldaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa aaaaaaaworldaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaworldaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
+        </Typography>
+      </FormModal>
+    </>
   )
 }
