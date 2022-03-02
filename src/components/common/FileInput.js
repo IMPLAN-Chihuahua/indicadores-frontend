c60849f4a2578663b9f082e6e9921cafcfc6e219
Luis Cruz <luisccc99@gmail.com>
2022-03-02 15:22:43 -0700

    Finish user form, add validation scheme for creation and create component to handle files with drag and drop or selection

---
+++ src/components/common/FileInput.js
@@ -0,0 +1,80 @@
+import { Typography } from "@mui/material";
+import { useCallback, useEffect } from "react";
+import { useDropzone } from "react-dropzone";
+import { useFormContext } from "react-hook-form";
+
+
+const FileInput = (props) => {
+  const { name, label } = props;
+  const { register, unregister, setValue, watch } = useFormContext();
+  const files = watch(name);
+
+  const onDrop = useCallback((droppedFiles) => {
+    setValue(name, droppedFiles, { shouldValidate: true });
+  }, [setValue, name]);
+
+  const { getRootProps, getInputProps, isDragActive } = useDropzone({
+    onDrop,
+    accept: props.accept
+  });
+
+  useEffect(() => {
+    register(name);
+    return () => {
+      unregister(name);
+    }
+  }, [register, unregister, name]);
+
+  return (
+    <>
+      <label className=" " htmlFor={name}>
+        {label}
+      </label>
+      <div
+        {...getRootProps()}
+        type='file'
+        role='button'
+        aria-label='Subir archivo'
+        id={name}
+      >
+        <input {...props} {...getInputProps()} />
+        <div
+          style={{
+            width: '100%',
+            border: 'dashed 2px lightgrey',
+            display: 'flex',
+            justifyContent: 'center',
+            borderRadius: 5,
+            alignItems: 'center',
+            flexDirection: 'column',
+            backgroundColor: isDragActive ? '#f8f8f8' : 'white'
+          }}>
+          <Typography pt={3} pb={3}>Arrastra la imagen de perfil para subirla</Typography>
+          {
+            !!files?.length && (
+              <div>
+                {
+                  files.map((file, index) => {
+                    return (
+                      <div key={index}>
+                        <img
+                          src={URL.createObjectURL(file)}
+                          alt={file.name}
+                          style={{
+                            height: '150px'
+                          }} />
+                      </div>
+                    )
+                  })
+                }
+              </div>
+            )
+          }
+        </div>
+      </div>
+    </>
+  );
+
+}
+
+export default FileInput;
--- src/components/dashboard/forms/user/FormUser.js
+++ src/components/dashboard/forms/user/FormUser.js
@@ -2,28 +2,37 @@ import CssBaseline from '@mui/material/CssBaseline';
 import Box from '@mui/material/Box';
 import TextField from '@mui/material/TextField';
 import Grid from '@mui/material/Grid';
-import { Controller, useForm } from 'react-hook-form';
+import { Controller, FormProvider, useForm } from 'react-hook-form';
 import { FormControl, InputLabel, MenuItem, Select, Switch } from '@mui/material';
 import FormGroup from '@mui/material/FormGroup';
 import FormControlLabel from '@mui/material/FormControlLabel';
 import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
 import Container from '@mui/material/Container';
+import FileInput from '../../../common/FileInput';
+import { yupResolver } from '@hookform/resolvers/yup';
+import { createUserSchema } from '../../../../utils/userValidator';
 
 const FormUser = () => {
-  const { control, handleSubmit } = useForm({
+  const methods = useForm({
+    mode: 'onBlur',
     defaultValues: {
       correo: '',
-      password: '',
-      confirmPassword: '',
+      clave: '',
+      confirmClave: '',
       activo: true,
       idRol: 2,
       nombres: '',
       apellidoPaterno: '',
       apellidoMaterno: ''
-    }
+    },
+    resolver: yupResolver(createUserSchema)
   });
-  const onSubmit = (data) => console.log(data);
+
+  const onSubmit = (data) => {
+    const { confirmClave, ...user } = data
+    console.log(user)
+  }
 
   return (
     <Container sx={{
@@ -31,165 +40,181 @@ const FormUser = () => {
         sm: 'sm',
       }
     }}>
-      <CssBaseline />
-      <Box
-        component='form'
-        onSubmit={handleSubmit(onSubmit)}
-        noValidate
-      >
-        <Grid container columnSpacing={3} rowSpacing={2}>
-          <Grid item xs={12}>
-            <Typography variant="h6" component="h3">
-              Cuenta
-            </Typography>
-          </Grid>
-          <Grid item xs={12}>
-            <Box
-              backgroundColor='aliceBlue'
-              height={150}
-            >
-            </Box>
-          </Grid>
-          <Grid item sm={12}>
-            <Controller
-              name="correo"
-              control={control}
-              render={({
-                field: { onChange, value }
-              }) => (
-                <TextField
-                  label='Correo'
-                  type='email'
-                  onChange={onChange}
-                  value={value}
-                  fullWidth
-                />
-              )} />
-          </Grid>
-          <Grid item xs={12} sm={6}>
-            <Controller
-              name="password"
-              control={control}
-              render={({
-                field: { onChange, value }
-              }) => (
-                <TextField
-                  label='Contraseña'
-                  type='password'
-                  onChange={onChange}
-                  value={value}
-                  fullWidth
-                />
-              )} />
-          </Grid>
-          <Grid item xs={12} sm={6}>
-            <Controller
-              name="confirmPassword"
-              control={control}
-              render={({
-                field: { onChange, value }
-              }) => (
-                <TextField
-                  label='Confirmar Contraseña'
-                  type='password'
-                  onChange={onChange}
-                  value={value}
-                  fullWidth
-                />
-              )} />
-          </Grid>
-          <Grid item xs={12} sm={6}>
-            <Controller
-              name="idRol"
-              defaultValue={2}
-              control={control}
-              render={({ field }) => (
-                <FormControl fullWidth>
-                  <InputLabel id='rol-label'>Rol</InputLabel>
-                  <Select
-                    id='id-rol'
-                    labelId='rol-label'
-                    label='Rol'
-                    {...field}
-                  >
-                    <MenuItem value={1}>Administrador</MenuItem>
-                    <MenuItem value={2}>Usuario</MenuItem>
-                  </Select>
-                </FormControl>
-              )}
-            />
-          </Grid>
-          <Grid item xs={12} sm={6}>
-            <Controller
-              name="activo"
-              control={control}
-              render={({ field }) => (
-                <FormGroup>
-                  <FormControlLabel
-                    control={<Switch {...field} defaultChecked={true} />}
-                    label="Activo" />
-                </FormGroup>
-              )} />
-          </Grid>
-          <Grid item xs={12} sx={{ mt: 2 }}>
-            <Typography variant="h6" component="h3">
-              Información Básica
-            </Typography>
-          </Grid>
-          <Grid item sm={12}>
-            <Controller
-              name="nombres"
-              control={control}
-              render={({
-                field: { onChange, value }
-              }) => (
-                <TextField
-                  label='Nombres'
-                  type='text'
-                  onChange={onChange}
-                  value={value}
-                  fullWidth
-                />
-              )} />
-          </Grid>
-          <Grid item xs={12} sm={6}>
-            <Controller
-              name="apellidoPaterno"
-              control={control}
-              render={({
-                field: { onChange, value }
-              }) => (
-                <TextField
-                  label='Apellido Paterno'
-                  type='text'
-                  onChange={onChange}
-                  value={value}
-                  fullWidth
-                />
-              )} />
-          </Grid>
-          <Grid item xs={12} sm={6}>
-            <Controller
-              name="apellidoMaterno"
-              control={control}
-              render={({
-                field: { onChange, value }
-              }) => (
-                <TextField
-                  label='Apellido Materno'
-                  type='text'
-                  onChange={onChange}
-                  value={value}
-                  fullWidth
-                />
-              )} />
-          </Grid>
-          <Grid item xs={12} sx={{ display: 'flex', justifyContent: 'flex-end' }}>
-            <Button type='submit' variant='contained'>Crear</Button>
-          </Grid>
+      <FormProvider {...methods}>
+        <Box
+          component='form'
+          onSubmit={methods.handleSubmit(onSubmit)}
+          noValidate
+        >
+          <Grid container columnSpacing={3} rowSpacing={2}>
+            <Grid item xs={12}>
+              <Typography variant="h6" component="h3">
+                Cuenta
+              </Typography>
+            </Grid>
+            <Grid item xs={12}>
+              <FileInput
+                accept='image/png, image/jpg, image/jpeg, image/gif'
+                name='profileImage'
+                label='Subir Archivo'
+              />
+            </Grid>
+            <Grid item sm={12}>
+              <Controller
+                name="correo"
+                control={methods.control}
+                render={({
+                  field: { onChange, value },
+                  fieldState: { error }
+                }) => (
+                  <TextField
+                    label='Correo'
+                    type='email'
+                    error={!!error}
+                    helperText={error ? error.message : null}
+                    onChange={onChange}
+                    value={value}
+                    fullWidth
+                  />
+                )} />
+            </Grid>
+            <Grid item xs={12} sm={6}>
+              <Controller
+                name="clave"
+                control={methods.control}
+                render={({
+                  field: { onChange, value },
+                  fieldState: { error }
+                }) => (
+                  <TextField
+                    label='Contraseña'
+                    type='password'
+                    error={!!error}
+                    helperText={error ? error.message : null}
+                    onChange={onChange}
+                    value={value}
+                    fullWidth
+                  />
+                )} />
+            </Grid>
+            <Grid item xs={12} sm={6}>
+              <Controller
+                name="confirmClave"
+                control={methods.control}
+                render={({
+                  field: { onChange, value },
+                  fieldState: { error }
+                }) => (
+                  <TextField
+                    label='Confirmar Contraseña'
+                    type='password'
+                    error={!!error}
+                    helperText={error ? error.message : null}
+                    onChange={onChange}
+                    value={value}
+                    fullWidth
+                  />
+                )} />
+            </Grid>
+            <Grid item xs={12} sm={6}>
+              <Controller
+                name="idRol"
+                defaultValue={2}
+                control={methods.control}
+                render={({ field }) => (
+                  <FormControl fullWidth>
+                    <InputLabel id='rol-label'>Rol</InputLabel>
+                    <Select
+                      id='id-rol'
+                      labelId='rol-label'
+                      label='Rol'
+                      {...field}
+                    >
+                      <MenuItem value={1}>Administrador</MenuItem>
+                      <MenuItem value={2}>Usuario</MenuItem>
+                    </Select>
+                  </FormControl>
+                )}
+              />
+            </Grid>
+            <Grid item xs={12} sm={6}>
+              <Controller
+                name="activo"
+                control={methods.control}
+                render={({ field }) => (
+                  <FormGroup>
+                    <FormControlLabel
+                      control={<Switch {...field} defaultChecked={true} />}
+                      label="Activo" />
+                  </FormGroup>
+                )} />
+            </Grid>
+            <Grid item xs={12} sx={{ mt: 2 }}>
+              <Typography variant="h6" component="h3">
+                Información Básica
+              </Typography>
+            </Grid>
+            <Grid item sm={12}>
+              <Controller
+                name="nombres"
+                control={methods.control}
+                render={({
+                  field: { onChange, value },
+                  fieldState: { error }
+                }) => (
+                  <TextField
+                    label='Nombres'
+                    type='text'
+                    error={!!error}
+                    helperText={error ? error.message : null}
+                    onChange={onChange}
+                    value={value}
+                    fullWidth
+                  />
+                )} />
+            </Grid>
+            <Grid item xs={12} sm={6}>
+              <Controller
+                name="apellidoPaterno"
+                control={methods.control}
+                render={({
+                  field: { onChange, value },
+                  fieldState: { error }
+                }) => (
+                  <TextField
+                    label='Apellido Paterno'
+                    type='text'
+                    error={!!error}
+                    helperText={error ? error.message : null}
+                    onChange={onChange}
+                    value={value}
+                    fullWidth
+                  />
+                )} />
+            </Grid>
+            <Grid item xs={12} sm={6}>
+              <Controller
+                name="apellidoMaterno"
+                control={methods.control}
+                render={({
+                  field: { onChange, value }
+                }) => (
+                  <TextField
+                    label='Apellido Materno'
+                    type='text'
+                    onChange={onChange}
+                    value={value}
+                    fullWidth
+                  />
+                )} />
+            </Grid>
+            <Grid item xs={12} sx={{ display: 'flex', justifyContent: 'flex-end' }}>
+              <Button type='submit' variant='contained'>Crear</Button>
+            </Grid>
 
-        </Grid>
-      </Box>
+          </Grid>
+        </Box>
+      </FormProvider>
     </Container>
   );
 }
