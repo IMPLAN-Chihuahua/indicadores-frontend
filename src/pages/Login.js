6df0f31205e16997e7ab29250bef727119bad5f8
Luis Cruz <luisccc99@gmail.com>
2022-03-17 15:44:57 -0700

    Refactor login to use alert context and display any error message

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,7 +1,6 @@
 import { Fade } from '@mui/material'
-import { Box } from '@mui/system'
 import React, { useState, useEffect } from 'react'
-import Copyright from '../components/Copyright'
+import { Alert } from '../components/common/Alert'
 import LoginComponent from '../components/LoginComponent'
 import './styles/login.css'
 
@@ -9,25 +8,25 @@ export const Login = () => {
     const [showLogin, setShowLogin] = useState(false);
 
     useEffect(() => {
-      setShowLogin(true)
-      return () => {
-        setShowLogin(false)
-     }
+        setShowLogin(true)
+        return () => {
+            setShowLogin(false)
+        }
     }, [])
-    
 
-  return (
-    <div className='login'>
-        <Fade in={showLogin} timeout={1500}>
-          <div className='login-container-left'></div>
-        </Fade>
-        <Fade in={showLogin} timeout={1500}>
-          <div className='login-container-right'>
-          <div className='login-component'>
-          <LoginComponent/>
-          </div>
-          </div>
-        </Fade>
-    </div>
-  )
+    return (
+        <div className='login'>
+            <Fade in={showLogin} timeout={1500}>
+                <div className='login-container-left'></div>
+            </Fade>
+            <Fade in={showLogin} timeout={1500}>
+                <div className='login-container-right'>
+                    <div className='login-component'>
+                        <LoginComponent />
+                    </div>
+                </div>
+            </Fade>
+            <Alert />
+        </div>
+    )
 }
