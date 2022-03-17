032fea13e9826200b258f35b937334edda644bbe
Luis Cruz <luisccc99@gmail.com>
2022-03-17 12:55:10 -0700

    Refactor dialog file (change spacing from 8 spaces to 4) and add close modal in modules' page

--- src/components/dashboard/common/FormDialog.js
+++ src/components/dashboard/common/FormDialog.js
@@ -1,17 +1,11 @@
 import {
-    Grid,
     Dialog,
-    Typography,
     Slide,
-    AppBar,
-    Toolbar,
-    IconButton,
 } from '@mui/material';
 import React from 'react';
-import CloseIcon from '@mui/icons-material/Close';
 
 const Transition = React.forwardRef(function Transition(props, ref) {
-    return <Slide direction="up" ref={ref} {...props} />;
+    return <Slide direction="down" ref={ref} {...props} />;
 });
 
 const ModalModelo = ({ open, setOpenModal, children, title }) => {
@@ -20,26 +14,10 @@ const ModalModelo = ({ open, setOpenModal, children, title }) => {
     return (
         <>
             <Dialog
-                fullScreen
                 open={open}
                 onClose={handleClose}
                 TransitionComponent={Transition}
             >
-                <AppBar sx={{ position: 'relative', bgcolor: 'black'}}>
-                    <Toolbar>
-                        <IconButton
-                            edge="start"
-                            color="inherit"
-                            onClick={handleClose}
-                            aria-label="close"
-                        >
-                            <CloseIcon />
-                        </IconButton>
-                        <Typography sx={{ ml: 2, flex: 1 }} variant="h6" component="div">
-                            {title}
-                        </Typography>
-                    </Toolbar>
-                </AppBar>
                 {children}
             </Dialog>
         </>
