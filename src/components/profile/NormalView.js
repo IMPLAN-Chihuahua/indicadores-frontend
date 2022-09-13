3740529d079ae13fabe836264aa3ee8b39b4fb5f
Miguel Valdez <mike.281299@gmail.com>
2022-09-13 11:31:07 -0600

    Added user.urlImage to user profile

--- src/components/profile/NormalView.js
+++ src/components/profile/NormalView.js
@@ -4,13 +4,13 @@ import '../../pages/styles/profile.css'
 import { Avatar, Button, Grid, Typography } from '@mui/material';
 
 export const NormalView = ({ user }) => {
-
+  console.log(user);
   return (
     <Box className='bottom-centered'>
       <Grid container className='profile-grid animate__animated animate__fadeInRight'>
         <Grid item xs={12} md={12} className='profile-grid-item'>
           <Box className='user-image-container'>
-            <Avatar className='user-image' sx={{ width: 170, height: 170 }} src={`http://localhost:8080/${user.urlImagen}`}>
+            <Avatar className='user-image' sx={{ width: 170, height: 170 }} src={`${user.urlImagen}`}>
             </Avatar>
             <h1 className='user-role'>{
               user.roles === 'ADMIN' ? 'Admin' : 'Usuario'
@@ -18,7 +18,7 @@ export const NormalView = ({ user }) => {
           </Box>
         </Grid>
         <Grid item xs={12} md={12} className='profile-grid-item'>
-          <Typography variant='h4' className='user-name'>
+          <Typography variant='h5' className='user-name'>
             {user.nombres} {user.apellidoPaterno}
           </Typography>
         </Grid>
