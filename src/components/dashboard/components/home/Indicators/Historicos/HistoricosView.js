f055dd3bfd62357ba12e5f8d4d6b421eb437ace7
Miguel Valdez <mike.281299@gmail.com>
2022-10-07 15:18:40 -0600

    Orderer historicos by its date

--- src/components/dashboard/components/home/Indicators/Historicos/HistoricosView.js
+++ src/components/dashboard/components/home/Indicators/Historicos/HistoricosView.js
@@ -13,6 +13,9 @@ import DeleteForeverIcon from '@mui/icons-material/DeleteForever';
 
 import './historicos.css';
 import { useParams } from 'react-router-dom';
+import Swal from 'sweetalert2';
+import FormDialog from '../../../../common/FormDialog';
+import FormHistoricos from './FormHistoricos';
 
 export const HistoricosView = () => {
   let perPage = localStorage.getItem('perPage') || 5;
@@ -27,8 +30,18 @@ export const HistoricosView = () => {
   const [sortBy, setSortBy] = useState('id');
   const [clickInfo, setClickInfo] = useState([]);
 
+
+  const [openModal, setOpenModal] = useState(false);
+  const handleOpenModal = () => {
+    setOpenModal(true)
+  };
+
+  const handleCloseModal = () => {
+    setOpenModal(false);
+  };
+
   const isMounted = useRef(true);
-  const { historicosList, isLoading, isError } = useHistoricos(
+  const { historicosList, isLoading, isError, mutate } = useHistoricos(
     perPaginationCounter,
     paginationCounter,
     id,
@@ -36,13 +49,24 @@ export const HistoricosView = () => {
     order,
   );
 
-  const handleDeleteHistorico = (idHistorico) => {
-    deleteHistorico(idHistorico);
-    window.location.reload();
-  }
-
-  const handleClickInfo = (data) => {
-    console.log(data);
+  const handleDeleteHistorico = ({ id }) => {
+    Swal.fire({
+      title: '¿Deseas eliminar este valor histórico?',
+      text: 'Al eliminar este registro, dejará de ser visible en la tabla de valores históricos de Chihuahua Métrica y en el sistema de gestión de Chihuahua en Datos.',
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonColor: '#F14156',
+      cancelButtonColor: '#B7BFCC',
+      cancelButtonText: '<div style="color: #7A7A7A; font-weight: 600; font-family: sans-serif">Cancelar, conservar registro</div>',
+      confirmButtonText: '<div style="font-weight: 500; font-family: sans-serif">Aceptar, eliminar registro</div>',
+    }).then((result) => {
+      if (result.isConfirmed) {
+        deleteHistorico(id);
+        Swal.fire('Eliminado!', 'El registro ha sido eliminado.', 'success');
+        mutate();
+      }
+    });
+    // deleteHistorico(id);
   }
 
   historicosList && (totalPages = historicosList.totalPages);
@@ -55,6 +79,7 @@ export const HistoricosView = () => {
         ...rowsHistoricosEdited,
         {
           id: data.id,
+          anio: data.anio,
           valor: data.valor,
           fuente: data.fuente,
         },
@@ -76,6 +101,7 @@ export const HistoricosView = () => {
           ...rowsHistoricosEdited,
           {
             id: data.id,
+            anio: data.anio,
             valor: data.valor,
             fuente: data.fuente,
           },
@@ -93,8 +119,8 @@ export const HistoricosView = () => {
 
   const columns = [
     {
-      field: 'id',
-      headerName: 'ID',
+      field: 'anio',
+      headerName: 'Año de referencia',
       flex: 0.5,
       headerClassName,
       sortable,
@@ -136,8 +162,7 @@ export const HistoricosView = () => {
               <span
                 className="dt-action-delete"
                 onClick={() => {
-                  // setOpenModal((prev) => !prev);
-                  handleClickInfo(params.row);
+                  handleDeleteHistorico(params.row);
                 }}
               >
                 <DeleteForeverIcon />
@@ -146,8 +171,8 @@ export const HistoricosView = () => {
             <span
               className="dt-action-edit"
               onClick={() => {
-                // setOpenModal((prev) => !prev);
-                // setClickInfo(params.row);
+                handleOpenModal();
+                setClickInfo({ type: 1, ...params.row });
               }}
             >
               <ModeEditIcon />
@@ -182,7 +207,7 @@ export const HistoricosView = () => {
             </Grid>
             <br />
             <Box className='add-historico'>
-              <Button variant='contained'>
+              <Button variant='contained' onClick={() => { handleOpenModal(); setClickInfo({ type: 2 }); }}>
                 Agregar histórico
               </Button>
             </Box>
@@ -194,6 +219,13 @@ export const HistoricosView = () => {
               total={historicosList.total}
               isLoading={isLoading}
             />
+
+            <FormDialog
+              open={openModal}
+              setOpenModal={setOpenModal}
+            >
+              <FormHistoricos type={clickInfo.type} id={clickInfo.id} anio={clickInfo.anio} valor={clickInfo.valor} fuente={clickInfo.fuente} handleCloseModal={handleCloseModal} mutate={mutate} />
+            </FormDialog>
           </>
         )
       }
