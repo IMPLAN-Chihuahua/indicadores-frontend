c48f4d6559ffe06df2d61c36c3d03010e20e089b
Luis Cruz <luisccc99@gmail.com>
2022-10-10 10:45:34 -0600

    Refactor status component to use MUI chips

--- src/components/dashboard/common/Status.js
+++ src/components/dashboard/common/Status.js
@@ -1,30 +1,28 @@
-import React from 'react'
+import { Chip } from '@mui/material';
 import CheckCircleIcon from '@mui/icons-material/CheckCircle';
 import CancelIcon from '@mui/icons-material/Cancel';
 import PersonIcon from '@mui/icons-material/Person';
 import GppGoodIcon from '@mui/icons-material/GppGood';
-import ArrowUpwardIcon from '@mui/icons-material/ArrowUpward';
 import ArrowDownwardIcon from '@mui/icons-material/ArrowDownward';
 
-import './common.css'
-export const Status = ({status}) => {
-  return (
-    <>
-{
-     (status == 'Activo' || status == 'SI' )
-     ?<div className='dt-chip-active'> <CheckCircleIcon sx={{color:'#10A04E', paddingRight:'10px'}}/> <span className='dt-chip-active--text'>Activo</span></div>
-     :(status == 'Inactivo' || status == 'NO') 
-     ?<div className='dt-chip-inactive'><CancelIcon sx={{color:'#9E1010',paddingRight:'5px'}}/> <span className='dt-chip-inactive--text'>Inactivo</span></div> 
-     :(status == 'Administrador')
-     ?<div className='dt-chip-admin'><GppGoodIcon sx={{color:'#01579b',paddingRight:'5px'}}/> <span className='dt-chip-admin--text'>Administrador</span></div> 
-     :(status == 'Usuario')
-     ?<div className='dt-chip-user'><PersonIcon sx={{color:'#546e7a',paddingRight:'5px'}}/> <span className='dt-chip-user--text'>Usuario</span></div>
-     :(status == 'DESCENDENTE')
-     ?<div><ArrowDownwardIcon/></div> 
-     :(status == 'ASCENDENTE')
-     ?<div><ArrowDownwardIcon/></div> 
-     :<h3>N/A</h3>
-}   
-    </>
-  )
+export const Status = ({ status }) => {
+  const formattedStatus = status.toLowerCase();
+  switch (formattedStatus) {
+    case 'activo':
+    case 'si':
+      return <Chip label='Activo' variant='outlined' color='success' icon={<CheckCircleIcon />} />
+    case 'inactivo':
+    case 'no':
+      return <Chip label='Inactivo' variant='outlined' color='error' icon={<CancelIcon />} />
+    case 'administrador':
+      return <Chip label='Administrador' variant='outlined' color='primary' icon={<GppGoodIcon />} />
+    case 'usuario':
+      return <Chip label='Usuario' variant='outlined' color='default' icon={<PersonIcon />} />
+    case 'descendente':
+      return <div><ArrowDownwardIcon /></div>;
+    case 'ascendente':
+      return <div><ArrowDownwardIcon /></div>;
+    default:
+      return <Chip variant='outlined' label={status} />
+  }
 }
