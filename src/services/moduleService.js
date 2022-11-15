f1116ebc1dd2eb28b2107dea4e3298089435407f
Luis Cruz <luisccc99@gmail.com>
2022-11-15 13:46:04 -0600

    Add function service to update a tema, and update query to return temas sorted by the updatedAt field

--- src/services/moduleService.js
+++ src/services/moduleService.js
@@ -4,18 +4,22 @@ export const createModule = async (modulo) => {
   try {
     const response = await protectedApi.post('/modulos', modulo);
     return response.data;
-
   } catch (error) {
     throw error;
   };
 }
+
+export const updateModule = (id, modulo) => {
+  return protectedApi.put(`/modulos/${id}`, modulo);
+}
+
 export const toggleTemaStatus = (id) => {
   return protectedApi.post(`/modulos/${id}/toggle-status`);
 }
 
 export const getTemas = async () => {
   try {
-    const res = await protectedApi.get('/modulos');
+    const res = await protectedApi.get('/modulos?sortBy=updatedAt&order=DESC');
     const { data: temas } = res.data;
     return temas;
   } catch (error) {
