7daaf1b7108cb4e7dc3e992d637f65440329c880
Miguel Valdez <mike.281299@gmail.com>
2022-11-15 11:54:52 -0600

    Add checkbox input for adding new users into selection for responsability when handling an indicador

--- src/components/dashboard/components/home/Responsables/Relation.js
+++ src/components/dashboard/components/home/Responsables/Relation.js
@@ -3,7 +3,8 @@ import React from 'react'
 import { useState } from 'react';
 import { useEffect } from 'react';
 import { useParams } from 'react-router-dom'
-import { getRelationUsers } from '../../../../../services/usuarioIndicadorService';
+import { getUsersThatDoesntHaveRelation, useRelationUsers } from '../../../../../services/usuarioIndicadorService';
+import { getLastedUsers } from '../../../../../services/userService';
 import DatagridTable from '../../../common/DatagridTable';
 import { Status } from '../../../common/Status';
 import Checkbox from '@mui/material/Checkbox';
@@ -11,35 +12,51 @@ import Autocomplete from '@mui/material/Autocomplete';
 import CheckBoxOutlineBlankIcon from '@mui/icons-material/CheckBoxOutlineBlank';
 import CheckBoxIcon from '@mui/icons-material/CheckBox';
 import PersonalLoader from '../../../../common/PersonalLoader/PersonalLoader';
+import FormDuration from '../../../forms/relationship/FormDuration';
+import FormDialog from '../../../common/FormDialog';
 import './responsables.css';
-
 
 const Relation = () => {
   const { id } = useParams();
-  const [indicador, setIndicador] = useState();
   const [loading, setLoading] = useState(false);
+  const [users, setUsers] = useState([]);
+  const [params, setParams] = useState([]);
 
-  const indicadorFetcher = async () => {
-    const indicador = await getRelationUsers(id);
-    return indicador;
+  const usersFetcher = async () => {
+    const { data } = await getUsersThatDoesntHaveRelation(id)
+    return data;
   };
+
+  const setUsersArray = async () => {
+    setLoading(true);
+    const users = await usersFetcher();
+    setUsers(users);
+    setLoading(false);
+  };
+
+
+  const { indicador, isLoading, hasError, mutate } = useRelationUsers(id);
+  const [openModal, setOpenModal] = useState(false);
+  const handleOpenModal = () => {
+    setOpenModal(true)
+  };
+
+  const handleCloseModal = () => {
+    setOpenModal(false);
+  };
+
 
   const icon = <CheckBoxOutlineBlankIcon fontSize="small" />;
   const checkedIcon = <CheckBoxIcon fontSize="small" />;
 
   useEffect(() => {
-    indicadorFetcher()
-      .then(res => {
-        setIndicador(res.data);
-        setLoading(true);
-      });
-  }, [id])
+    setUsersArray();
+  }, [])
 
   const headerClassName = "dt-theme--header";
   const sortable = false;
   const headerAlign = "center";
   const align = "center";
-
   const columns = [
     {
       field: 'id',
@@ -117,17 +134,14 @@ const Relation = () => {
     },
   ];
 
-  const top100Films = [
-    { title: 'John Doe', year: 1994 },
-    { title: 'Kellen Daniel Kellen', year: 1968 },
-    { title: 'Ivy Pagac Ivy', year: 1998 },
-    { title: 'Carroll Hayes', year: 2014 },
-  ];
+  const clearUsersSelectedParams = () => {
+    setParams([]);
+  }
 
   return (
     <>
       {
-        loading ?
+        !isLoading ?
           <>
             <Box className='responsables-header '>
               <h1>Indicador: {indicador?.nombre}</h1>
@@ -135,9 +149,14 @@ const Relation = () => {
                 <Autocomplete
                   multiple
                   id="checkboxes-tags-demo"
-                  options={top100Films}
+                  options={users}
                   disableCloseOnSelect
-                  getOptionLabel={(option) => option.title}
+                  onChange={(event, value) => {
+                    setParams(value);
+                  }}
+                  value={params}
+                  filterSelectedOptions
+                  getOptionLabel={(option) => option.nombres}
                   renderOption={(props, option, { selected }) => (
                     <li {...props}>
                       <Checkbox
@@ -146,15 +165,17 @@ const Relation = () => {
                         style={{ marginRight: 8 }}
                         checked={selected}
                       />
-                      {option.title}
+                      {option.nombres}
                     </li>
                   )}
                   style={{ width: 500 }}
                   renderInput={(params) => (
-                    <TextField {...params} label="Checkboxes" placeholder="Favorites" />
+                    <TextField {...params} variant="outlined" label="Usuarios" placeholder="Usuarios" />
                   )}
                 />
-                <Button variant='contained'>+</Button>
+                <Box className='responsables-button'>
+                  <Button variant='contained' disabled={params.length === 0 ? true : false} onClick={() => { handleOpenModal(); }}>Agregar</Button>
+                </Box>
               </Box>
             </Box>
 
@@ -166,6 +187,19 @@ const Relation = () => {
               pageSize={10}
               perPage={10}
             />
+
+            <Box className='responsables-footer'>
+              <Button variant='contained'>Cancelar</Button>
+              <Button variant='contained'>Guardar</Button>
+            </Box>
+
+
+            <FormDialog
+              open={openModal}
+              handleClose={() => setOpenModal(false)}
+            >
+              <FormDuration users={params} handleCloseModal={handleCloseModal} setUsersArray={setUsersArray} mutate={mutate} clearUsersSelectedParams={clearUsersSelectedParams} />
+            </FormDialog>
           </>
           :
           <PersonalLoader color="#1976D2" />
