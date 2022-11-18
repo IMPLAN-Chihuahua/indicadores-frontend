9056f16c770e284f0eea72eeae418ef73f0bda62
Luis Cruz <luisccc99@gmail.com>
2022-11-18 15:17:38 -0600

    Add content to map tab, refactor form mapa, and

--- src/components/dashboard/components/home/Indicators/Mapa/MapView.js
+++ src/components/dashboard/components/home/Indicators/Mapa/MapView.js
@@ -1,66 +1,201 @@
-import { Button } from '@mui/material';
-import React from 'react'
-import { Fragment } from 'react';
-import { createRef } from 'react';
-import { useState } from 'react'
-import { useScreenshot } from 'use-react-screenshot'
-import ScreenCapture from '../../../../../../utils/ScreenCapture';
+import { Box, Button, DialogActions, DialogContent, DialogTitle, Grid, Paper, Stack, Typography } from "@mui/material"
+import { useCallback, useEffect, useState } from "react"
+import { useParams } from "react-router-dom"
+import { IndicadorProvider } from "../../../../../../contexts/IndicadorContext"
+import useIsMounted from "../../../../../../hooks/useIsMounted"
+import { createMapa, getMapa } from "../../../../../../services/indicatorService"
+import { updateMapa } from "../../../../../../services/mapaService"
+import { showAlert } from "../../../../../../utils/alert"
+import PersonalLoader from "../../../../../common/PersonalLoader/PersonalLoader"
+import FormDialog from "../../../../common/FormDialog"
+import { defaultMapa, FormMapa } from "../../../../forms/mapa/FormMapa"
 
 const MapView = () => {
-  const [screenCapture, setScreenCapture] = useState(false)
-
-  const handleScreenCapture = (screenCapture) => {
-    setScreenCapture(screenCapture);
+  const { id: idIndicador } = useParams();
+  const [mapa, setMapa] = useState({});
+  const [isLoading, setLoading] = useState(false);
+  const [open, setOpen] = useState(false);
+  const isMounted = useIsMounted();
+  const parseMapToFormData = mapObj => {
+    const fd = new FormData();
+    for (const field in mapObj) {
+      if (field === 'urlImagen') {
+        fd.append(field, mapObj[field][0] || null);
+        continue;
+      }
+      if (mapObj[field]) {
+        fd.append(field, mapObj[field]);
+        continue;
+      }
+    }
+    return fd;
+  };
+  const handleUpdate = (data) => {
+    const parsed = parseMapToFormData(data);
+    showAlert({
+      title: '¿Deseas actualizar el mapa de este indicador?',
+      text: `Al actualizar este registro, los cambios generados se actualizarán
+       en Chihuahua Métrica y en el sistema de gestión de Chihuahua en Datos.`,
+      icon: 'question',
+      showCancelButton: true
+    })
+      .then(option => {
+        if (option.isConfirmed) {
+          return updateMapa(mapa.id, parsed);
+        }
+      })
+      .then(res => {
+        if (res) {
+          showAlert({
+            title: 'Actualización existosa',
+            icon: 'success',
+            text: 'El mapa del indicador ha sido actualizado'
+          });
+          fetchMapa();
+        }
+      })
+      .catch(err => {
+        showAlert({
+          title: 'Hubo un error',
+          text: err,
+          icon: 'error'
+        })
+      })
   }
 
+  const handleCreate = (data) => {
+    const parsed = parseMapToFormData(data);
+    showAlert({
+      title: '¿Deseas crear el mapa para este indicador?',
+      text: `Al crear este registro, los cambios generados se actualizarán
+       en Chihuahua Métrica y en el sistema de gestión de Chihuahua en Datos.`,
+      icon: 'question',
+      showCancelButton: true
+    }).then(option => {
+      if (option.isConfirmed) {
+        return createMapa(idIndicador, parsed);
+      }
+    })
+      .then(res => {
+        if (res) {
+          showAlert({
+            title: 'Mapa creado exitosamente',
+            icon: 'success',
+            text: 'El mapa del indicador ha sido creado'
+          })
+          setOpen(false)
+          fetchMapa();
+        }
+      })
+      .catch(err => {
+        showAlert({
+          title: 'Hubo un error',
+          text: err,
+          icon: 'error'
+        })
+      })
+  }
 
-  const [message, setMessage] = useState('https://www.openstreetmap.org/export/embed.html?bbox=-0.004017949104309083%2C51.47612752641776%2C0.00030577182769775396%2C51.478569861898606&layer=mapnik');
+  const fetchMapa = useCallback(() => {
+    setLoading(true);
+    getMapa(idIndicador)
+      .then(res => {
+        if (isMounted()) {
+          if (res.data.data) {
+            const mapObj = res.data.data;
+            setMapa({
+              ...mapObj,
+              ...(mapObj.urlImagen && { urlImagen: [mapObj.urlImagen] })
+            });
+          }
+        }
+      })
+      .catch(err => {
+        showAlert({
+          title: 'Hubo un error',
+          icon: 'error',
+          text: err
+        })
+      })
+      .finally(_ => setLoading(false))
+  }, [idIndicador, isMounted])
 
-  const handleChange = event => {
-    setMessage(event.target.value);
-  };
-
-  const ref = createRef(null)
-  const [image, takeScreenshot] = useScreenshot()
-  const getImage = () => takeScreenshot(ref.current)
-
+  useEffect(() => {
+    fetchMapa();
+  }, [fetchMapa])
+  
   return (
-    <ScreenCapture onEndCapture={handleScreenCapture}>
-      {({ onStartCapture }) => (
-        <Fragment>
-          <input
-            type="text"
-            id="message"
-            name="message"
-            onChange={handleChange}
-            value={message}
-          />
+    <>
+      <Box
+        className='indicator'
+        sx={{ overflow: 'scroll', flex: '1 1 auto', height: '500px' }}
+        padding={3}
+      >
+        {
+          isLoading ? (<PersonalLoader />)
+            : Object.keys(mapa).length > 0
+              ? (
+                <Stack direction='column' gap={1} height='100%'>
+                  <Paper
+                    component={Grid}
+                    item
+                    padding={3}
+                  >
 
-          <div>
-            <div>
-              <button style={{ marginBottom: '10px' }} onClick={getImage}>
-                Take screenshot
-              </button>
-            </div>
-            <img width={"600"} src={image} alt={'Screenshot'} />
-            {/* <div ref={ref}>
-              <iframe
-                id="inlineFrameExample"
-                title="Inline Frame Example"
-                width="1200"
-                height="600"
-                src={message}>
-              </iframe>
-            </div> */}
-          </div>
-          <button onClick={onStartCapture}>Capture</button>
-          <br />
-          <br />
-          <img src={screenCapture} />
-        </Fragment>
-      )}
-    </ScreenCapture>
-
+                    <IndicadorProvider
+                      indicador={{ mapa }}
+                      onSubmit={handleUpdate}
+                    >
+                      <FormMapa defaultTitle={false} />
+                      <DialogActions>
+                        <Button type='submit' form='form-mapa' variant='contained'>Guardar</Button>
+                      </DialogActions>
+                    </IndicadorProvider>
+                  </Paper>
+                  <Paper
+                    component={Stack}
+                    direction='column'
+                    padding={3}
+                    height='100%'
+                  >
+                    <Typography variant='h5' mb={2}>Vista del dashboard</Typography>
+                    <iframe
+                      src={mapa.url}
+                      width='100%'
+                      height='500px'
+                      style={{ border: '1px solid #d2d2d2', borderRadius: '5px' }}>
+                    </iframe>
+                  </Paper>
+                </Stack>
+              )
+              : (
+                <Paper
+                  sx={{ p: 2 }}
+                >
+                  <Typography mb={1} variant='body1'>Este indicador no cuenta con un mapa</Typography>
+                  <Button variant='contained' onClick={() => setOpen(true)}>Agregar Mapa</Button>
+                </Paper>
+              )
+        }
+      </Box>
+      <FormDialog
+        open={open}
+        handleClose={() => setOpen(false)}
+      >
+        <IndicadorProvider
+          indicador={{ mapa: defaultMapa }}
+          onSubmit={handleCreate}
+        >
+          <DialogContent>
+            <FormMapa defaultTitle />
+          </DialogContent>
+          <DialogActions>
+            <Button onClick={() => setOpen(false)} variant='text'>Cancelar</Button>
+            <Button type='submit' form='form-mapa' variant='contained'>Guardar</Button>
+          </DialogActions>
+        </IndicadorProvider>
+      </FormDialog>
+    </>
   )
 }
 
