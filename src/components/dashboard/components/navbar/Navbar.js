392e6bb971ebbc8777d6b764b844845acf2be943
Luis Cruz <luisccc99@gmail.com>
2022-11-23 14:48:40 -0600

    Refactor design of navbar (add gradient to background color of navbar)

--- src/components/dashboard/components/navbar/Navbar.js
+++ src/components/dashboard/components/navbar/Navbar.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import './navbar.css'
 import ExitToAppIcon from '@mui/icons-material/ExitToApp';
-import { Box, Avatar } from '@mui/material';
+import { Box, Avatar, Typography } from '@mui/material';
 import { Link } from 'react-router-dom';
 import { useAuth } from '../../../../contexts/AuthContext';
 import { nameConstructor } from '../../../../utils/nameValidator';
@@ -10,40 +10,29 @@ export const Navbar = () => {
 
   const { user, handleLogOut } = useAuth();
   return (
-    <>
-      <Box className='navbar'>
-        <Box className='navbar-wrapper'>
-          <Box className='navbar-left'>
-            <Link to='/' className='link-to-profile'>
-              {/* <img src='logo_chihuahua_metrica.webp' alt='logo' className='logo' style={{ width: 200 }} /> */}
-              Chihuahua en Datos
-            </Link>
-          </Box>
-          <Box className='navbar-right'>
-
-            <Box className='navbar-rigth-options'>
-
-              <Box className='navbar-option'>
-
-                <Avatar className='navbar-option-icon' src={`${user.urlImagen}`} sx={{ width: 40, height: 40 }} />
-
-                <div className='navbar-user-info'>
-                  <span className='navbar-option-text element'>{`${nameConstructor(user.nombres, user.apellidoPaterno)}`}</span>
-                  <span className='navbar-option-text element-caption'>{`${user.roles === 'ADMIN' ? 'Administrador' : 'Usuario'}`}</span>
-                </div>
-
-              </Box>
-
-              <Box className='navbar-option-close' onClick={handleLogOut}>
-                <ExitToAppIcon className='navbar-option-icon' />
-                <span className='navbar-option-text'>Cerrar sesión</span>
-              </Box>
+    <Box className='navbar'>
+      <Box className='navbar-wrapper'>
+        <Box className='navbar-left'>
+          <Link to='/' className='link-to-profile'>
+            Chihuahua en Datos
+          </Link>
+        </Box>
+        <Box className='navbar-right'>
+          <Box className='navbar-rigth-options'>
+            <Box className='navbar-option'>
+              <Avatar className='navbar-option-icon' src={`${user.urlImagen}`} sx={{ width: 40, height: 40 }} />
+              <div className='navbar-user-info'>
+                <Typography>{`${nameConstructor(user.nombres, user.apellidoPaterno)}`}</Typography>
+                <Typography variant='caption'>{`${user.roles === 'ADMIN' ? 'Administrador' : 'Usuario'}`}</Typography>
+              </div>
+            </Box>
+            <Box className='navbar-option-close' onClick={handleLogOut}>
+              <ExitToAppIcon className='navbar-option-icon' />
+              <span className='navbar-option-text'>Cerrar sesión</span>
             </Box>
           </Box>
         </Box>
-
       </Box>
-    </>
-
+    </Box>
   )
 }
