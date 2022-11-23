501ffbd3cf01d1175ddff640682a0ded8d790122
Luis Cruz <luisccc99@gmail.com>
2022-11-23 12:33:32 -0600

    Refacto style of components in home page

--- src/components/dashboard/components/home/userInformation/UserInformation.js
+++ src/components/dashboard/components/home/userInformation/UserInformation.js
@@ -2,7 +2,7 @@ import { Box } from "@mui/system";
 import React, { useState } from "react";
 import "./userInformation.css";
 import { useAuth } from '../../../../../contexts/AuthContext';
-import { Avatar, DialogTitle } from "@mui/material";
+import { Avatar, DialogTitle, IconButton, Paper, Stack, Typography } from "@mui/material";
 import SettingsIcon from '@mui/icons-material/Settings';
 import FormDialog from "../../../common/FormDialog";
 import FormUser, { FORM_USER_ACTIONS } from "../../../forms/user/FormUser";
@@ -10,7 +10,7 @@ import PersonalLoader from "../../../../common/PersonalLoader/PersonalLoader";
 
 export const UserInformation = () => {
   const { user } = useAuth();
-  const { correo, nombres, apellidoPaterno, apellidoMaterno } = user;
+  const { correo, nombres, apellidoPaterno, apellidoMaterno, descripcion } = user;
   const fullName = `${nombres} ${apellidoPaterno} ${apellidoMaterno || ''}`;
   const [openModal, setOpenModal] = useState(false);
 
@@ -23,25 +23,37 @@ export const UserInformation = () => {
   }
 
   return (
-    <Box className='user-big-container'>
-      <Box className="user-general-container">
-        <Box className="user-container">
-          <Box className="user-picture">
-            <Box className="picture">
-              <Avatar alt="Remy Sharp" src={user.urlImagen} sx={{ height: 120, width: 120 }} />
-            </Box>
-          </Box>
-          <Box className="user-information">
-            <h2 className="user-name">{fullName}</h2>
-            <span className="user-email">{correo}</span>
-          </Box>
+    <>
+      <Paper
+        component={Stack}
+        direction={{ md: 'row', xs: 'column' }}
+        elevation={1}
+        sx={{
+          mb: 3,
+          p: 2,
+          position: 'relative'
+        }}
+      >
+        <Avatar
+          variant='rounded'
+          alt={user.nombres}
+          src={user.urlImagen || user.nombres}
+          sx={{
+            height: 120,
+            width: 120,
+            alignSelf: 'center'
+          }} />
+        <Box ml={{ md: 2 }}>
+          <Typography variant='h5'>{fullName}</Typography>
+          <Typography variant='body1'>{correo}</Typography>
+          <Typography variant='body2'>{descripcion}</Typography>
         </Box>
-        <Box className="date" onClick={() => setOpenModal(true)}>
-          <Box className="date-text">
+        <Box position='absolute' top={2} right={2}>
+          <IconButton onClick={() => setOpenModal(true)}>
             <SettingsIcon />
-          </Box>
+          </IconButton>
         </Box>
-      </Box>
+      </Paper>
       <FormDialog
         open={openModal}
         handleClose={handleCloseModal}
@@ -53,6 +65,6 @@ export const UserInformation = () => {
           selectedUser={user}
         />
       </FormDialog>
-    </Box>
+    </>
   );
 };
