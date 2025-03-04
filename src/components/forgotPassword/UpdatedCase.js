30a0adbce4ed6c6a707f507dd4caa19d868bcaee
Luis Cruz <luisccc99@gmail.com>
2025-03-04 12:11:06 -0600

    Use yarn to manage packages, and update breaking changes for jwt-decode package

--- src/components/forgotPassword/UpdatedCase.js
+++ src/components/forgotPassword/UpdatedCase.js
@@ -1,5 +1,5 @@
 import { Button } from "@mui/material";
-import jwtDecode from "jwt-decode";
+import { jwtDecode } from "jwt-decode";
 import React, { useState } from "react";
 import { HashLoader } from "react-spinners";
 import { publicApi } from "../../services";
@@ -8,8 +8,8 @@ import PassField from "../common/passField/PassField";
 import "./FpCases.css";
 import { SuccessCase } from "./SuccessCase";
 
-export const UpdatedCase = ({token}) => {
-  const {correo} = (jwtDecode(token).user)
+export const UpdatedCase = ({ token }) => {
+  const { correo } = (jwtDecode(token).user)
 
   const [message, setMessage] = useState(null);
   const [error, setError] = useState(null);
@@ -29,21 +29,21 @@ export const UpdatedCase = ({token}) => {
       .then((res) => {
         if (res) {
 
-          if(formData.password == formData.confirmPassword){
-            publicApi.patch(`/auth/password-reset/${token}`,JSON.stringify({clave: formData.password}))
-            .then( res => {
-              setMessage("");
-              setError("");
-              setLoading(false)
-              setSuccess(true)
-            })
-            .catch( err => {
-              console.log(err)
-              setMessage("");
-              setError("Ocurrio un error");
-              setLoading(false)
-            })
-          }else{
+          if (formData.password == formData.confirmPassword) {
+            publicApi.patch(`/auth/password-reset/${token}`, JSON.stringify({ clave: formData.password }))
+              .then(res => {
+                setMessage("");
+                setError("");
+                setLoading(false)
+                setSuccess(true)
+              })
+              .catch(err => {
+                console.log(err)
+                setMessage("");
+                setError("Ocurrio un error");
+                setLoading(false)
+              })
+          } else {
             setMessage("");
             setError("Las contraseñas no coinciden");
             setLoading(false)
@@ -62,34 +62,34 @@ export const UpdatedCase = ({token}) => {
 
   return (
     <>
-    {
-      (!success)
-      ?
-      <div className="fpc-container-all">
-      {(loading) && <div className='loading'><HashLoader/></div>}
-        <form onSubmit={handleSubmit} className="fpc-container">
-          <div className="fpc-header">
-          <h1 style={{ textAlign: "center"}}>Recuperación de cuenta</h1>
-          <h3 style={{ textAlign: "center", color: "rgb(0,0,255,0.6)" }}>{`"${correo}"`}</h3>
-          </div>
+      {
+        (!success)
+          ?
+          <div className="fpc-container-all">
+            {(loading) && <div className='loading'><HashLoader /></div>}
+            <form onSubmit={handleSubmit} className="fpc-container">
+              <div className="fpc-header">
+                <h1 style={{ textAlign: "center" }}>Recuperación de cuenta</h1>
+                <h3 style={{ textAlign: "center", color: "rgb(0,0,255,0.6)" }}>{`"${correo}"`}</h3>
+              </div>
 
-          <p>Introduce la nueva contraseña</p>
-          {/* <TextField label='Contraseña' variant='standard' className='fpc-textfield'/>  */}
-          <PassField />
-          <p>Introduce otra vez la nueva contraseña</p>
-          {/* <TextField label='Confirmar contraseña' variant='standard' className='fpc-textfield'/>   */}
-          <PassField />
-          {message && <p style={{color: 'rgb(0,0,0,0.7)'}}>{message}</p>}
-          {error && <p style={{color: 'rgb(255,0,0,0.7)'}}>{error}</p>}
-          <div className="btn-recover-account">
-            <Button variant="outlined" type="submit">
-              Cambiar contraseña
-            </Button>
+              <p>Introduce la nueva contraseña</p>
+              {/* <TextField label='Contraseña' variant='standard' className='fpc-textfield'/>  */}
+              <PassField />
+              <p>Introduce otra vez la nueva contraseña</p>
+              {/* <TextField label='Confirmar contraseña' variant='standard' className='fpc-textfield'/>   */}
+              <PassField />
+              {message && <p style={{ color: 'rgb(0,0,0,0.7)' }}>{message}</p>}
+              {error && <p style={{ color: 'rgb(255,0,0,0.7)' }}>{error}</p>}
+              <div className="btn-recover-account">
+                <Button variant="outlined" type="submit">
+                  Cambiar contraseña
+                </Button>
+              </div>
+            </form>
           </div>
-        </form>
-      </div>
-      : <SuccessCase/>
-    }
+          : <SuccessCase />
+      }
     </>
   );
 };
--- src/pages/ForgotPassword.js
+++ src/pages/ForgotPassword.js
@@ -1,11 +1,11 @@
 import { Fade } from '@mui/material';
-import React, {useState, useEffect} from 'react'
-import { Link, Link as RouterLink} from 'react-router-dom';
+import React, { useState, useEffect } from 'react'
+import { Link, Link as RouterLink } from 'react-router-dom';
 import { ErrorCase } from '../components/forgotPassword/ErrorCase';
 import { InitialCase } from '../components/forgotPassword/InitialCase'
 import { UpdatedCase } from '../components/forgotPassword/UpdatedCase';
 import './styles/forgotPassword.css'
-import jwt from 'jwt-decode'
+import { jwtDecode } from 'jwt-decode'
 
 export const ForgotPassword = () => {
     const [showFP, setShowFP] = useState(false)
@@ -20,60 +20,60 @@ export const ForgotPassword = () => {
      * 3 - invalid token
      */
     const tokenIsValid = (params) => {
-        const {sub,iat,exp} = params;
+        const { sub, iat, exp } = params;
         const expirationDate = new Date(0).setUTCSeconds(exp)
         const currentDate = new Date();
 
-        if(expirationDate > currentDate){
+        if (expirationDate > currentDate) {
             return true
-        }else{
+        } else {
             return false
         }
     }
 
-    try{
+    try {
         (!token)
-        ? opt = 1
-        : (tokenIsValid(jwt(token)))
-        ? opt = 2 
-        : opt = 3
-    }catch{
+            ? opt = 1
+            : (tokenIsValid(jwtDecode(token)))
+                ? opt = 2
+                : opt = 3
+    } catch {
         opt = 3
     }
-        
+
     useEffect(() => {
         setShowFP(true)
         return () => {
-        setShowFP(false)
-       }
-      }, [])
-
-  return (
-      <>
-      <Fade in={showFP} timeout={1500}>
-    <div className='fp-container'>
-        <div className='fp-content'>
-        
-        {
-            (opt == 1)
-            ?
-            <>
-            <InitialCase/>
-            </>
-            :(opt == 2)
-            ?<UpdatedCase token={token} />
-            :(opt == 3)
-            ?
-            <>
-            <ErrorCase/>
-            </>
-            :<></>
-
+            setShowFP(false)
         }
+    }, [])
 
-        </div>
-    </div>
-      </Fade>
-      </>
-  )
+    return (
+        <>
+            <Fade in={showFP} timeout={1500}>
+                <div className='fp-container'>
+                    <div className='fp-content'>
+
+                        {
+                            (opt == 1)
+                                ?
+                                <>
+                                    <InitialCase />
+                                </>
+                                : (opt == 2)
+                                    ? <UpdatedCase token={token} />
+                                    : (opt == 3)
+                                        ?
+                                        <>
+                                            <ErrorCase />
+                                        </>
+                                        : <></>
+
+                        }
+
+                    </div>
+                </div>
+            </Fade>
+        </>
+    )
 }
