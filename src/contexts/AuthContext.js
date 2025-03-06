4e284c696a96ac233fab202c74e3a80fd8b8e0d0
Luis Cruz <luisccc99@gmail.com>
2025-03-06 09:48:23 -0600

    Use lazy initializers to avoid rerenderings componentes

--- src/contexts/AuthContext.js
+++ src/contexts/AuthContext.js
@@ -5,18 +5,9 @@ import { getCurrentUser } from '../services/userService';
 const AuthContext = createContext();
 
 const AuthProvider = ({ children }) => {
-  const [user, setUser] = useState();
+  const [user, setUser] = useState(() => JSON.parse(localStorage.getItem('indicadores-user')) || null);
   const [isLoading, setIsLoading] = useState(false);
   const [hasError, setHasError] = useState(false);
-
-  useEffect(() => {
-    const currentUser = JSON.parse(localStorage.getItem('indicadores-user'));
-    if (currentUser) {
-      setUser(currentUser)
-    } else {
-      handleLogOut();
-    }
-  }, []);
 
   const handleLogin = async (payload, onSuccess, onError) => {
     setIsLoading(true);
