182c34fb71d183a97471aa5a4c05b4e38aaa3d19
Luis Cruz <luisccc99@gmail.com>
2025-03-07 13:57:46 -0600

    Initialize values of form filtro with values from query params

--- src/components/dashboard/forms/indicador/FormFiltro.js
+++ src/components/dashboard/forms/indicador/FormFiltro.js
@@ -1,10 +1,12 @@
 import { Autocomplete, Box, Button, Checkbox, DialogActions, DialogContent, DialogTitle, FormControlLabel, Grid, TextField } from '@mui/material';
-import React from 'react'
+import React, { useCallback, useEffect, useState } from 'react'
 import { Controller, useForm, useWatch } from 'react-hook-form';
 import { useAuth } from '../../../../contexts/AuthContext';
 import { useObjetivos } from '../../../../services/objetivoService';
 import { useTemas } from '../../../../services/temaService';
 import { useUsers } from '../../../../services/userService';
+import { useSearchParams } from 'react-router-dom';
+import qs from 'qs'
 
 const defaultValues = {
   objetivos: [],
@@ -13,13 +15,14 @@ const defaultValues = {
   usuario: null,
   me: false,
 }
+
 const FormFiltro = (props) => {
   const { handleClose, submitCallback } = props;
   const { objetivos, isLoading: isObjetivosLoading } = useObjetivos();
   const { temas, isLoading: isTemasLoading } = useTemas();
   const { users: usuarios, isLoading: isUsuariosLoading } = useUsers();
   const { user } = useAuth();
-
+  const [searchParams, _] = useSearchParams();
   const methods = useForm({ defaultValues });
 
   const { control, handleSubmit, reset } = methods;
@@ -30,22 +33,55 @@ const FormFiltro = (props) => {
     const objetivos = data.objetivos.map(o => o.id);
     const temas = data.temas.map(t => t.id);
     const usuarios = meValue ? [] : data.usuarios.map(u => u.id)
-    const hasActiveFilters = owner !== null || [objetivos, temas, usuarios].some(arr => arr.length > 0)
 
     submitCallback({
-      owner,
-      objetivos,
-      temas,
-      usuarios,
-      hasActiveFilters
+      ...(owner !== null && { owner }),
+      ...(objetivos.length > 0 && { objetivos }),
+      ...(usuarios.length > 0 && { usuarios }),
+      ...(temas.length > 0 && { temas })
     });
     handleClose();
   };
 
   const clearFilter = () => {
     reset(defaultValues);
-    submitCallback({ owner: null, objetivos: [], temas: [], usuarios: [], hasActiveFilters: false })
+    submitCallback(null)
   };
+
+  useEffect(() => {
+    const initialValues = qs.parse(searchParams.toString());
+    if (!initialValues) return;
+
+    let {
+      temas: temasFromParams = [],
+      objetivos: objetivosFromParams = [],
+      usuarios: usuariosFromParams = [],
+      owner = null,
+    } = initialValues;
+
+    const findItemById = (itemsFromParams, items) => {
+      const foundItems = [];
+      for (const itemId of itemsFromParams) {
+        const found = items.find(i => i.id === parseInt(itemId))
+        if (!found) continue;
+        foundItems.push(found)
+      }
+      return foundItems;
+    }
+
+    const foundObjetivos = findItemById(objetivosFromParams, objetivos);
+    const foundTemas = findItemById(temasFromParams, temas);
+    const foundUsuarios = findItemById(usuariosFromParams, usuarios);
+
+    reset({
+      objetivos: foundObjetivos,
+      temas: foundTemas,
+      usuarios: foundUsuarios,
+      ...(owner !== null && { owner: parseInt(owner), me: true }),
+    })
+
+
+  }, [temas, usuarios, objetivos]);
 
   return (
     <Box component={'form'} noValidate onSubmit={handleSubmit(onSubmit)}>
@@ -53,7 +89,6 @@ const FormFiltro = (props) => {
       <DialogContent>
         <Box>
           <Grid container spacing={2}>
-
             <Grid item xs={12}>
               <Grid container>
                 <Grid item xs={12} sx={{ mb: 1 }}>
