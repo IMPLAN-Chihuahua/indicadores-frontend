dc74a07ec0e24be2905ba23b76fe1002086df924
Luis Cruz <luisccc99@gmail.com>
2025-03-07 13:50:41 -0600

    Use lazy loading in useState to initialize value of user

--- src/contexts/AuthContext.js
+++ src/contexts/AuthContext.js
@@ -5,7 +5,7 @@ import { getCurrentUser } from '../services/userService';
 const AuthContext = createContext();
 
 const AuthProvider = ({ children }) => {
-  const [user, setUser] = useState(() => JSON.parse(localStorage.getItem('indicadores-user')) || null);
+  const [user, setUser] = useState(() => JSON.parse(localStorage.getItem('indicadores-user')));
   const [isLoading, setIsLoading] = useState(false);
   const [hasError, setHasError] = useState(false);
 
@@ -41,10 +41,6 @@ const AuthProvider = ({ children }) => {
     const curr = await getCurrentUser();
     setUser(prev => ({ ...prev, ...curr }));
   }, [])
-
-  useEffect(() => {
-    localStorage.setItem('indicadores-user', JSON.stringify(user));
-  }, [user])
 
   return (
     <AuthContext.Provider
